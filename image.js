93cb02490c38fc0f936f14bf0e6e3ef0d30467fe
Metro31 <53766948+Metro31@users.noreply.github.com>
2022-11-08 16:21:56 +0300

    PCA

---
+++ image.js
@@ -0,0 +1,5 @@
+<script src="/highslide.js" type="text/javascript"></script>
+<link href="/highslide.css" rel="stylesheet" property="stylesheet" />
+<script type="text/javascript">
+  hs.graphicsDir = '/graphics/';
+</script>
